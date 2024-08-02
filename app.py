b000c13027645cc735f952d6feaa46df0c1d1693
Ishani <ishani.chakraborty2005@gmail.com>
2024-08-02 14:27:20 +0530

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = "flower123456"
 
-@app.route('/home')
-def home():
-    return "Hello world"
+@app.route("/")
+def start():
+    return "The MBSA Server is Running"
+
+@app.route("/mbsa")
+def mbsa():
+    return render_template('index.html')
