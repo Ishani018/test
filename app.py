60e6dfa3d4cf6a18fe241b811f4246bf29fcc753
Ishani <ishani.chakraborty2005@gmail.com>
2024-08-02 15:17:57 +0530

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ import os
 app = Flask(__name__)
 app.secret_key = "flower123456"
 
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['DOWNLOAD_FOLDER'] = 'downloads'
 
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+if not os.path.exists(app.config['DOWNLOAD_FOLDER']):
+    os.makedirs(app.config['DOWNLOAD_FOLDER'])
 
 @app.route("/")
 def home():
