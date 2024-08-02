48dc8edacdfa899fcc157e74e0d213cd4a4db8aa
Ishani <ishani.chakraborty2005@gmail.com>
2024-08-02 15:08:55 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect, send_file,url_for
 import os
 
 app = Flask(__name__)
