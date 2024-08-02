6440cc49ea15812c54877038b8f94d61d9fcc363
Ishani <ishani.chakraborty2005@gmail.com>
2024-08-02 11:34:22 +0530

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = "flower123456"
 
 @app.route('/home')
 def home():
-    return render_template("base.html")
+    return "Hello world"
